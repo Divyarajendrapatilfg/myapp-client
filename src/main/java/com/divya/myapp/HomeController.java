dafb0202d1171e2a8db830a168c52ca6287c036f
Divyarajendrapatilfg <divyarajendrapatil@farviewglobal.com>
2025-07-22 15:49:47 +0530

    Create HomeController.java

---
+++ src/main/java/com/divya/myapp/HomeController.java
@@ -0,0 +1,13 @@
+package com.divya.myapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to MyApp! Config loaded successfully.";
+    }
+}
