3d6de7e6a978eae0fdb7ec105a939780d0ba64c0
Divyarajendrapatilfg <divyarajendrapatil@farviewglobal.com>
2025-07-22 15:51:11 +0530

    Update HomeController.java

--- src/main/java/com/divya/myapp/HomeController.java
+++ src/main/java/com/divya/myapp/HomeController.java
@@ -1,13 +1,17 @@
 package com.divya.myapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String home() {
-        return "Welcome to MyApp! Config loaded successfully.";
+        return welcomeMessage;
     }
 }
